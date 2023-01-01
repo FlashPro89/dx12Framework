0769505d24a1d836e56d924132068c6a86e3739c
flash <flash@unigine.com>
2023-01-01 23:44:25 +0400

    fixed project settings

---
+++ samples/ParallaxMappingSample/EntryPoint.cpp
@@ -0,0 +1,9 @@
+#include "ParallaxMappingSample.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine, int nCmdShow)
+{
+    ParallaxMappingSample sample("ParallaxMappingSample", DX12Framework::DX12FRAMEBUFFERING::DX12FB_TRIPLE);
+    DX12Framework::run(&sample);
+
+	return 0;
+}
